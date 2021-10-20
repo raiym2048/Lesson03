22f26ee87df12a794bf9632baa430abc29e83132
raiym2048 <raiymbek.daniiaruulu@iaau.edu.kg>
2021-10-20 14:43:21 +0600

    фф
    фв

--- app/src/main/java/com/example/lesson03/signin.kt
+++ app/src/main/java/com/example/lesson03/signin.kt
@@ -1,5 +1,6 @@
 package com.example.lesson03
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,13 @@ class signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+    }
+    fun toscreen(view: android.view.View){
+        val intent = Intent(this, screenMain::class.java)
+        startActivity(intent)
+    }
+    fun tosignup(view: android.view.View){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
